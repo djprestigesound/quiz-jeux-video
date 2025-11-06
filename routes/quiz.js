3aaa00201b03c9ded2809925be403497fb38150d
Quentin Steffen <quentinsteffen@mac.home>
2025-11-06 22:17:41 +0100

    Add redirect for direct access to /quiz/start

--- routes/quiz.js
+++ routes/quiz.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const quizController = require('../controllers/quizController');
 
+// Redirection si accès direct à /quiz/start
+router.get('/start', (req, res) => {
+  res.redirect('/');
+});
+
 router.post('/start', quizController.start);
 router.get('/question', quizController.showQuestion);
 router.post('/answer', quizController.submitAnswer);
